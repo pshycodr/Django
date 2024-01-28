93a3ba176438b69c4c15ea2b9cf6963ce1ac9f70
pshycodr <115141578+pshycodr@users.noreply.github.com>
2024-01-28 10:32:40 +0530

    Delete .venv directory

--- .venv/django_project_1/login_page/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from.import views as VS
-
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-     path('loginform/', VS.registration ),
-]
--- .venv/django_project_1/login_page/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def registration(request):
-    username = request.POST.get('userName', '') 
-    fav_language= request.POST.get('fav_language','')
-    
-    print(username, fav_language )
-    return render(request, 'login_form.html')
